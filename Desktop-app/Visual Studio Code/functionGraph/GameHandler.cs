efcda9e7577b83bee3718892a491398bcf5817e8
Miskolczi Levente Tamás <miskolczi.levente@ckik.loc>
2025-09-12 13:50:43 +0200

    nemtom

---
+++ Desktop-app/Visual Studio Code/functionGraph/GameHandler.cs
@@ -0,0 +1,93 @@
+﻿
+using System;
+
+namespace Minesweeper
+{
+    abstract class GameHandler
+    {
+        protected Table table;
+        public abstract void StartGame();
+    }
+
+    class MinesweeperGame : GameHandler
+    {
+        private int size, bombs;
+
+        //public MinesweeperGame(int size, int bombs)
+        public MinesweeperGame()
+        {
+            this.size = size;
+            this.bombs = bombs;
+            table = new Table(size, bombs);
+        }
+
+        public override void StartGame()
+        {
+
+            Console.Write("Add meg a pálya méretét (pl. 8): ");
+            size = int.Parse(Console.ReadLine());
+            Console.Write("Add meg a bombák számát: ");
+            bombs = int.Parse(Console.ReadLine());
+
+            if (size > 10) 
+                size = 10;
+
+            if (bombs > (size * size - 1))
+                bombs = size * size - 1;
+
+            table = new Table(size, bombs);
+
+            bool running = true;
+            while (running)
+            {
+                table.PrintTable();
+                Console.WriteLine("Parancsok: r x y = reveal, f x y = flag, q = quit");
+                Console.Write("> ");
+                string input = Console.ReadLine();
+                if (input == null) continue;
+
+                var parts = input.Split(' ');
+                if (parts[0] == "q") break;
+
+                if (parts.Length < 3) continue;
+                int x = int.Parse(parts[1]);
+                int y = int.Parse(parts[2]);
+
+                if (parts[0] == "r")
+                {
+                    bool safe = table.RevealField(x, y);
+                    if (!safe)
+                    {
+                        table.RevealAll();           
+                        table.PrintTable();
+                        Console.WriteLine("💥 VESZTETTÉL! Bombára léptél!");
+                        running = false;
+                    }
+                }
+                else if (parts[0] == "f")
+                {
+                    table.ToggleFlag(x, y);
+                    if (table.CheckWin())
+                    {
+                        table.RevealAll();           
+                        table.PrintTable();
+                        Console.WriteLine("🎉 NYERTÉL! Minden bombát megjelöltél!");
+                        running = false;
+                    }
+                }
+
+            }
+
+
+
+            Console.WriteLine("Szeretnél új játékot? (i/n)");
+            string restart = Console.ReadLine();
+            if (restart.ToLower() == "i")
+            {
+                Program.Main(null);
+            }
+        }
+
+        
+    }
+}
---
+++ Desktop-app/Visual Studio Code/functionGraph/Table.cs
@@ -0,0 +1,161 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Minesweeper
+{
+    class Table
+    {
+        private Field[,] fields;
+        private int size;
+        private int bombCount;
+
+        public Table(int size, int bombCount)
+        {
+            this.size = size;
+            this.bombCount = bombCount;
+            fields = new Field[size, size];
+            GenerateTable();
+        }
+
+        private void GenerateTable()
+        {
+            Random rnd = new Random();
+            HashSet<(int, int)> bombs = new HashSet<(int, int)>();
+
+            while (bombs.Count < bombCount)
+            {
+                int x = rnd.Next(size);
+                int y = rnd.Next(size);
+                bombs.Add((x, y));
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (bombs.Contains((i, j)))
+                        fields[i, j] = new BombField();
+                    else
+                        fields[i, j] = new EmptyField();
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (fields[i, j] is EmptyField)
+                    {
+                        int bombsAround = CountAdjacentBombs(i, j);
+                        if (bombsAround > 0)
+                            fields[i, j] = new NumberField(bombsAround);
+                    }
+                }
+            }
+        }
+
+        private int CountAdjacentBombs(int x, int y)
+        {
+            int count = 0;
+            for (int dx = -1; dx <= 1; dx++)
+            {
+                for (int dy = -1; dy <= 1; dy++)
+                {
+                    if (dx == 0 && dy == 0) continue;
+                    int nx = x + dx, ny = y + dy;
+                    if (nx >= 0 && nx < size && ny >= 0 && ny < size)
+                    {
+                        if (fields[nx, ny] is BombField) count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        public bool RevealField(int x, int y)
+        {
+            if (!IsValid(x, y) || fields[x, y].IsRevealed) return true;
+
+            fields[x, y].Reveal();
+
+            if (fields[x, y] is EmptyField)
+            {
+                for (int i = x - 1; i <= x + 1; i++)
+                {
+                    for (int j = y - 1; j <= y + 1; j++)
+                    {
+                        if (IsValid(i, j))
+                        {
+                            RevealField(i, j); 
+                        }
+                    }
+                }
+            }
+
+            if (fields[x, y] is BombField)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void ToggleFlag(int x, int y)
+        {
+            if (IsValid(x, y) && !fields[x, y].IsRevealed)
+                fields[x, y].IsFlagged = !fields[x, y].IsFlagged;
+        }
+
+        public bool CheckWin()
+        {
+            int correctlyFlagged = 0, totalBombs = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (fields[i, j] is BombField)
+                    {
+                        totalBombs++;
+                        if (fields[i, j].IsFlagged) correctlyFlagged++;
+                    }
+                }
+            }
+            return correctlyFlagged == totalBombs;
+        }
+
+        private bool IsValid(int x, int y) => x >= 0 && x < size && y >= 0 && y < size;
+
+        public void PrintTable()
+        {
+            Console.Clear();
+            Console.Write("   ");
+            for (int j = 0; j < size; j++) Console.Write($"{j} ");
+            Console.WriteLine();
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"{i} ");
+                if (i < 10) Console.Write(" ");
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(fields[i, j].GetSymbol() + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void RevealAll()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    fields[i, j].Reveal();
+                }
+            }
+        }
+
+    }
+}
