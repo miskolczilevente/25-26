022c3af89f5b140e1e1f2995483f7b7e0c1a9aa5
Miskolczi Levente Tamás <miskolczi.levente@ckik.loc>
2025-09-26 14:06:45 +0200

    Wild West basic's

---
+++ Desktop-app/Visual Studio 22/Sheriff Beadandó/Wild West/Wild West/MapObjects/TownHall.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wild_West.Map
+{
+    abstract class TownHall : TownComponent
+    {
+        
+
+    }
+}
---
+++ Desktop-app/Visual Studio 22/Sheriff Beadandó/Wild West/Wild West/TownComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wild_West
+{
+    internal class TownComponent
+    {
+        int[,] MapMatrix = new int[25, 25];
+
+
+    }
+}
